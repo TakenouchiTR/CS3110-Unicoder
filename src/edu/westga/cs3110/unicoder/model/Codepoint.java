559ee5c4a16fb70650349225b8261381413d8029
TakenouchiTR <draconianlds@gmail.com>
2021-09-17 16:23:53 -0400

    Initial commit

---
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -0,0 +1,21 @@
+package edu.westga.cs3110.unicoder.model;
+
+public class Codepoint {
+	private String hexValue;
+	
+	public Codepoint(String hexValue) {
+		this.hexValue = hexValue;
+	}
+	
+	public String toUTF32() {
+		return null;
+	}
+	
+	public String toUTF16() {
+		return null;
+	}
+	
+	public String toUTF8() {
+		return null;
+	}
+}
---
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
@@ -0,0 +1,5 @@
+package edu.westga.cs3110.unicoder.tests.model.codepoint;
+
+public class TestConstructor {
+
+}
