866a56c73f79b06ee96c81ec2bcecfaa037fe6cc
TakenouchiTR <draconianlds@gmail.com>
2021-09-19 12:03:41 -0400

    Add positive tests for converting to UTF16

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF16.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF16.java
@@ -1,5 +1,38 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-public class TestToUTF16 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+public class TestToUTF16 {
+	@ParameterizedTest
+	@CsvSource( {"0000,0000", "D7FF,D7FF", "E000,E000", "FFFF,FFFF"} )
+	void testTwoBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		try {
+			assertEquals(expected.toLowerCase(), codepoint.toUTF16().toLowerCase(), "Check two byte UTF16");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@ParameterizedTest
+	@CsvSource( {"10000,D800DC00", "10FFFF,DBFFDFFF", "106DF4,DBDBDDF4"} )
+	void testFourBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		try {
+			assertEquals(expected.toLowerCase(), codepoint.toUTF16().toLowerCase(), "Check four byte UTF16");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
