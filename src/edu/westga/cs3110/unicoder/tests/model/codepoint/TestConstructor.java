957e6427bca09cf8cb930386d8f29a19421f87c9
TakenouchiTR <draconianlds@gmail.com>
2021-09-19 10:50:48 -0400

    Add test for out of bounds value

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
@@ -1,5 +1,21 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-public class TestConstructor {
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+public class TestConstructor {
+	@ParameterizedTest
+	@ValueSource(strings = {"FFFFFFFF", "110000"})
+	void testHexOutOfRange(String hexValue) {
+		assertThrows(
+				IllegalArgumentException.class, 
+				() -> {new Codepoint(hexValue);},
+				"Check if exception is thrown with out of bounds value"
+		);
+	}
 }
