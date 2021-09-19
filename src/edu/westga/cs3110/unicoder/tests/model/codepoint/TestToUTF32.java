95ac4e647ee5794e80c290a516576812ab2d9f9f
TakenouchiTR <draconianlds@gmail.com>
2021-09-19 12:21:10 -0400

    Add test for UTF32 decode

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF32.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF32.java
@@ -1,5 +1,18 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-public class TestToUTF32 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+public class TestToUTF32 {
+	@ParameterizedTest
+	@CsvSource( {"0000,00000000", "10FFFF,0010FFFF", "001A, 0000001A"} )
+	void testTwoBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		assertEquals(expected.toLowerCase(), codepoint.toUTF32().toLowerCase(), "Check UTF32 decode");
+	}
 }
