7ad46035af3b294309ee9a431d3a5e21a7e4790a
TakenouchiTR <draconianlds@gmail.com>
2021-09-19 10:42:21 -0400

    Add tests for three and four byte UTF8 decoding

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF8.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF8.java
@@ -24,4 +24,20 @@ public class TestToUTF8 {
 		
 		assertEquals(expected.toLowerCase(), codepoint.toUTF8().toLowerCase(), "Check two byte UTF8");
 	}
+	
+	@ParameterizedTest
+	@CsvSource( {"0800,E0A080", "FFFF,EFBFBF", "7AF9,E7ABB9"} )
+	void testThreeBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		assertEquals(expected.toLowerCase(), codepoint.toUTF8().toLowerCase(), "Check three byte UTF8");
+	}
+	
+	@ParameterizedTest
+	@CsvSource( {"10000,F0908080", "10FFFF,F48FBFBF", "106DF4,F486B7B4"} )
+	void testFourBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		assertEquals(expected.toLowerCase(), codepoint.toUTF8().toLowerCase(), "Check four byte UTF8");
+	}
 }
