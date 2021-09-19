5f2447395be58e8e65c69bf46b6356f17ff72757
TakenouchiTR <draconianlds@gmail.com>
2021-09-19 10:19:51 -0400

    Add tests for one and two byte UTF8

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF8.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF8.java
@@ -1,5 +1,27 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-public class TestToUTF8 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+public class TestToUTF8 {
+	@ParameterizedTest
+	@CsvSource( {"0000,00", "007F,7F", "0044,44"} )
+	void testOneByte(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		assertEquals(expected.toLowerCase(), codepoint.toUTF8().toLowerCase(), "Check one byte UTF8");
+	}
+	
+	@ParameterizedTest
+	@CsvSource( {"0080,C280", "07FF,DFBF", "01A6,C6A6"} )
+	void testTwoBytes(String hexValue, String expected) {
+		Codepoint codepoint = new Codepoint(hexValue);
+		
+		assertEquals(expected.toLowerCase(), codepoint.toUTF8().toLowerCase(), "Check two byte UTF8");
+	}
 }
